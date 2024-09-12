c4f8f4d446295b73ba68f2e3e6f6a1f180f3f0bc
naimsg <naimsaadigallego@gmail.com>
2024-09-12 11:12:35 +0200

    feat(main.py): Enrich printable text

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
+from rich.padding import Padding
+from rich.console import Console
+from rich.markdown import Markdown
 
 route = 'files/'
 dir_files = os.listdir(route)
-
 files = [f for f in dir_files if f.endswith('.txt')]
+console = Console()
 
-with open('./output.md','w') as output:
-    output.write('# Llista de gent que ha superat el taller: \n')
-    for file in files:
-        file_route = os.path.join(route, file)
-        with open(file_route, 'r') as input:
-            name = input.read()
-            output.write(f'* **{name}**\n')
-            
+def enrich (text):
+    return Padding(Markdown(text), (0,70))
+
+print()
+title = """ # Llista de gent que ha superat el taller: """
+rich_title = enrich(title)
+console.print(rich_title)
+for file in files:
+    file_route = os.path.join(route, file)
+    with open(file_route, 'r') as input:
+        name = input.read()
+        console.print(enrich(f"""* ### `{name}` amb l'arxiu `{file}` """))
+print()
